95b491cf84a86c6317f546d15b69c3858d19f94d
3yassu <68251430+3yassu@users.noreply.github.com>
2025-02-17 13:30:29 -0600

    Added isOperator function and precedence function
    isOperator checks if the character given is an operator
    precedence assigns each operator a precedence

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -15,24 +15,41 @@ template <typename T>
 class BinaryExpressionTree{
     private:
         BinaryNode<T>* root;
-    void sortExpression(BinaryNode<T> currentNode){
-    }
-    float evaluator(BinaryNode<T>* currentNode){
-        set<char> operators {'+', '-', '*', '/', '^'};
-        if(operators.find(currentNode->entry) == operators.end()){
-            return stof(currentNode->entry);
-        }else{
-            float left = evaluator(currentNode->left);
-            float right = evaluator(currentNode->right);
-            char oper = currentNode->entry[0];
-            float power = 1;
-            switch(oper){
-                case('+'): return(left + right);
-                case('-'): return(left - right);
-                case('*'): return(left * right);
-                case('/'): return(left / right);
-                case('^'): for(int i = 0; i < right; ++i){power *= left;}return(power);
+
+        void sortExpression(BinaryNode<T> currentNode){
+
+        }
+        int precedence(char oper){
+            if(oper == '^'){
+                return 3;
+            }else if(oper == '*' || oper == '/' || oper == '%'){
+                return 2;
+            }else if(oper == '+' || oper == '-'){
+                return 1;
+            }else{
+                throw runtime_error("Not a valid operator!")
+            }  
+        }
+        bool isOperator(char object){return if(object == '+' || object == '-' || object == '*' || object == '/' || object == '^' || object == '%' );}
+        float evaluator(BinaryNode<T>* currentNode){
+            if(!(isOperator(currentNode->entry))){
+                return stof(currentNode->entry);
+            }else{
+                float left = evaluator(currentNode->left);
+                float right = evaluator(currentNode->right);
+                char oper = currentNode->entry[0];
+                float power = 1;
+                switch(oper){
+                    case('+'): return(left + right);
+                    case('-'): return(left - right);
+                    case('*'): return(left * right);
+                    case('/'): return(left / right);
+                    case('^'): for(int i = 0; i < right; ++i){power *= left;}return(power);
+                    case('%'): return(left % right);
+                }
             }
         }
-    }
+    public:
+        string equation;
+        BinaryExpressionTree(string x) : equation(x) {}
 };
